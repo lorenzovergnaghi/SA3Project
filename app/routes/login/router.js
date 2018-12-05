6b3d0c6f225778a645cfed2ad234bab8d5e8ed9a
vergna96 <vergnaghilorenzo96@gmail.com>
2018-12-05 22:30:39 +0100

    login stuff

---
+++ app/routes/login/router.js
@@ -0,0 +1,26 @@
+/** @module favorite/login */
+'use strict';
+
+const http = require('http');
+const passport = require('passport');
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', function(req,res){
+  console.log('~GET LOGIN');
+  res.render('login');
+});
+
+
+
+router.post('/',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.writeHead(302,{'Location':'http://localhost:3000'}).end();
+  });
+
+
+
+module.exports = router;
