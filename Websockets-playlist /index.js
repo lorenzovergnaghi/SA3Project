bcf2e6ffa2c7bc05b6950e623cc445d2b5001191
Unknown <kicevm@usi.ch>
2018-12-11 19:46:47 +0100

    Chat - design + keyEvent

---
+++ Websockets-playlist /index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+
+
+//App setup
+var app = express();
+var server = app.listen(4000, function(){
+  console.log("Listening on port 4000!")
+});
+
+//static files
+app.use(express.static('public')) //show content of public folder on route root /
+
+//Socket setup
+var io = socket(server);
+io.on('connection', function(socket){
+  console.log("socket connection OK", socket.id);
+
+// Handle chat event
+  socket.on('chat', function(data){
+    io.sockets.emit("chat", data)
+  });
+  socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data)
+  })
+});
