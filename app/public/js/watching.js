0e64f9aa894eb4b7d84dc3ffca4976233e9f921e
vergna96 <vergnaghilorenzo96@gmail.com>
2018-12-12 12:05:09 +0100

    asd

--- app/public/js/watching.js
+++
@@ -1,5 +0,0 @@
-function setWatching(a,b){
-  document.querySelector('.bigone').src = a;
-  console.log(document.querySelector('.title_asdf'));
-  document.querySelector('.title_asdf').innerHTML =b;
-}
