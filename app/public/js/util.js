fd55a96ff800cc4e6b89019f7ce2a1015ebcecbe
Unknown <bonfij@usi.ch>
2018-12-13 16:13:38 +0100

    Finally the Watch button works

--- app/public/js/util.js
+++ app/public/js/util.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+    $(".img1").on({
+        mouseenter: function (e) {
+            $(e.target).css({"transform":"scale(1.1)", "opacity":"0.5"});
+        },
+        mouseleave: function(e){
+            $(e.target).css({"transform":"scale(1)", "opacity":"1"})
+        },
+    })
     $(".img2").on({
         mouseenter: function (e) {
             $(e.target).prev().css({"transform":"scale(1.1)", "opacity":"0.5"});
@@ -7,6 +15,7 @@ $(document).ready(function(){
             $(e.target).prev().css({"transform":"scale(1)", "opacity":"1"})
         },
         click:function(e){
+
         }
     })
 });
