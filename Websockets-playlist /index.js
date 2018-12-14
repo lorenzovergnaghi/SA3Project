ea165739799e8f22a38ad322b07cb21e3acb4ea7
Unknown <kicevm@usi.ch>
2018-12-14 10:05:05 +0100

    Bugs chat fixed + new feature

--- Websockets-playlist /index.js
+++ Websockets-playlist /index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var socket = require('socket.io');
 
 
-//App setup
+//We setup our App
 var app = express();
 var server = app.listen(4000, function(){
   console.log("Listening on port 4000!")
 });
 
-//static files
-app.use(express.static('public')) //show content of public folder on route root /
+//Show the content of public folder on route root /
+app.use(express.static('public'));
 
-//Socket setup
+//We setup the Socket
 var io = socket(server);
 io.on('connection', function(socket){
   console.log("socket connection done", socket.id);
@@ -22,16 +22,25 @@ io.on('connection', function(socket){
 
 
 
-// Handle chat event
+// Here on the server we handle the our events
   socket.on('chat', function(data){
     console.log(data);
     io.sockets.emit("chat", data)
   });
+  //Broadcast to all except one client.
+  //Because when I'm typing only the others must see that I'm typing a message.
   socket.on('typing', function(data){
     socket.broadcast.emit('typing', data)
   });
+  //Broadcast to all except one client.
   socket.on('notype', function () {
     socket.broadcast.emit('notype')
 
-  })
+  });
+  // We send only to one client, because when a client Send his message, only
+  // his input message is deleted and not also the other clients.
+  socket.on('afterSendingDelete', function () {
+    socket.emit('afterSendingDelete');
+  });
+
 });
--- Websockets-playlist /public/chat.js
+++ Websockets-playlist /public/chat.js
@@ -1,14 +1,15 @@
 // Make connection
-var socket = io.connect();
+// We can do that thanks to the script that we imported in index.html
+let socket = io.connect();
 
 //Query DOM
-var message = document.getElementById('message'),
+let message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     output = document.getElementById('output'),
     feedback = document.getElementById('feedback');
 
-
+// Function that generate a random Color
 function randomColorGenerator() {
   let values = [];
   for(let i = 0; i < 3;i++){
@@ -19,23 +20,29 @@ function randomColorGenerator() {
   return myRGB
 }
 
+// Name of the user takes a random Color
 handle.style.color = randomColorGenerator();
-message.style.color = randomColorGenerator();
 
 //Emit events
-//emit a message than the WebSockets on the Server
+//emit a message, than the WebSockets on the Server have to on this message.
+//Features: When we send a message to other clients, the message is deleted from the input message.
+//We send a message, the name of the User, and the color of the user name.
 btn.addEventListener('click', function() {
   if (message.value && handle.value) {
     socket.emit("chat", {
       message: message.value,
       handle: handle.value,
-      color_handle: handle.style.color,
-      color_message: message.style.color
+      color_handle: handle.style.color
     });
-    console.log(handle.style.color);
   }
+  if(message.value !== ""){
+    socket.emit('afterSendingDelete');
+  }
+
 });
 
+//Features: On keyup we can know when a user is typing.
+// When a user delete the message from the input message, the "is typing feature" disappear.
 message.addEventListener('keyup', function(){
   if(message.value !== ""){
     socket.emit('typing', handle.value);
@@ -48,8 +55,8 @@ message.addEventListener('keyup', function(){
 //Listen for addEventListener
 socket.on('chat', function(data){
   feedback.innerHTML = "";
-  output.innerHTML += "<strong><p style='color:"+data.color_handle+";'>"+data.handle+"</p></strong><p id='border' style='color:"+data.color_message+";'>"+data.message+"</p>"
-  message.value = "";
+  output.innerHTML += "<strong><p style='color:"+data.color_handle+";'>"+data.handle+"</p></strong><p>"+data.message+"</p>";
+  // message.value = "";
   document.getElementById('window').scrollTop = document.getElementById('window').scrollHeight
 });
 //
@@ -62,11 +69,16 @@ socket.on('typing', function(data){
 
 socket.on('notype', function () {
   feedback.innerHTML = "";
+});
+
+socket.on('afterSendingDelete', function () {
+  message.value = "";
+
 })
 
 
 
-// Trigger button click on enter
+// Trigger the button click event with the Enter button on keyboard (keyCode == 13)
 var input = document.getElementById("message");
 input.addEventListener("keyup", function(event) {
   event.preventDefault();
