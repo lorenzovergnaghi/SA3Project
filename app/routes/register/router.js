82c05c1c8ee881fc71c01d26968403425e61ca1d
vergna96 <vergnaghilorenzo96@gmail.com>
2018-12-15 17:01:08 +0100

    added register mechanism, checks if username is not used and if pasword is correct in both fields

--- app/routes/register/router.js
+++ app/routes/register/router.js
@@ -25,10 +25,33 @@ router.post('/',
     function(req,res){
       console.log(req.body);
       if (req.body.password[0] === req.body.password[1]) {
-        res.render('register', {x:req.body});
+        var cpas = "";
+        for (var i = 1; i <= req.body.password[0].length; i++) {
+          cpas += "*";
+        }
+        User.find({ username: req.body.lname }, function (err, user) {
+          if (err) {
+            console.log('error finding user in register');
+          }else {
+            if (user.length === 0) {
+              let newUser = new User({username : req.body.lname,password :req.body.password[0] ,email :req.body.email});
+              newUser.save(function(err,saved){
+                if (err) {
+                  console.log(err);
+                  res.redirect('login');
+                }else {
+                  res.redirect('login');
+                }
+              });
+            }else {
+              let msg = ["Username already in use"];
+              res.render('register', {x:{lname:req.body.lname, email:req.body.email,password:cpas},y:"Register operation failed : "+msg});
+            }
+          }
+        });
       }else {
-        let msg = ["passwords don't match",undefined];
-        res.render('register', {x:{lname:req.body.lname, email:req.body.email, password:msg}});
+        let msg = ["passwords don't match"];
+        res.render('register', {x:{lname:req.body.lname, email:req.body.email, password:req.body.password},y:"Register operation failed : "+msg});
       }
     });
 
